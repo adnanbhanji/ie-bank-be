cc7095572293e56d9c5c63d5701e51ecd038a000
Adnan Bhanji <bhanjiadnan786@gmail.com>
2023-10-14 14:20:09 +0200

    more uat changes

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 class Config(object):
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    APPINSIGHTS_INSTRUMENTATIONKEY = os.getenv('APPINSIGHTS_INSTRUMENTATIONKEY')
 
 class LocalConfig(Config):
     basedir = os.path.abspath(os.path.dirname(__file__))
@@ -25,6 +26,11 @@ class DevelopmentConfig(Config):
     )
     DEBUG = True
 
-class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+class UATConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.getenv('DBUSER'),
+    dbpass=os.getenv('DBPASS'),
+    dbhost=os.getenv('DBHOST'),
+    dbname=os.getenv('DBNAME')
+    )
     DEBUG = False

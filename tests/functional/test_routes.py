28c6af0f8b0c8cc3091df80c2c78128cbdda1bb9
Adnan Bhanji <bhanjiadnan786@gmail.com>
2023-10-14 14:56:27 +0200

    working uat

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -1,16 +1,14 @@
 from iebank_api import app
 import pytest
 
-
 def test_get_accounts(testing_client):
     """
     GIVEN a Flask application
     WHEN the '/accounts' page is requested (GET)
     THEN check the response is valid
     """
-    response = testing_client.get("/accounts")
+    response = testing_client.get('/accounts')
     assert response.status_code == 200
-
 
 def test_dummy_wrong_path():
     """
@@ -19,9 +17,8 @@ def test_dummy_wrong_path():
     THEN check the response is valid
     """
     with app.test_client() as client:
-        response = client.get("/wrong_path")
+        response = client.get('/wrong_path')
         assert response.status_code == 404
-
 
 def test_create_account(testing_client):
     """
@@ -29,39 +26,7 @@ def test_create_account(testing_client):
     WHEN the '/accounts' page is posted to (POST)
     THEN check the response is valid
     """
-    response = testing_client.post(
-        "/accounts", json={"name": "John Doe", "country": "Spain", "currency": "€"}
-    )
+    response = testing_client.post('/accounts', json={'name': 'John Doe', 'country': 'Spain', 'currency': '€'})
     assert response.status_code == 200
 
 
-def test_delete_account(testing_client):
-    """
-    GIVEN a Flask application
-    WHEN the '/accounts' page is deleted (DELETE)
-    THEN check the response is valid
-    """
-
-    # Create an account first
-    response = testing_client.post(
-        "/accounts", json={"name": "John Doe", "country": "Spain", "currency": "€"}
-    )
-
-    response = testing_client.delete("/accounts/1")
-    assert response.status_code == 200
-
-
-def test_get_account_by_id(testing_client):
-    """
-    GIVEN a Flask application
-    WHEN the '/accounts' page is requested (GET)
-    THEN check the response is valid
-    """
-
-    # Create an account first
-    response = testing_client.post(
-        "/accounts", json={"name": "John Doe", "country": "Spain", "currency": "€"}
-    )
-
-    response = testing_client.get("/accounts/1")
-    assert response.status_code == 200
